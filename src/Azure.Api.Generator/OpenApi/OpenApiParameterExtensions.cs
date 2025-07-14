51cd1cbe3f4ea8731365e502a67818b2e61be1a0
Fredrik Arvidsson <fresa@fresa.se>
2025-07-14 22:19:08 +0200

    add type declaration identifier for api parameters

---
+++ src/Azure.Api.Generator/OpenApi/OpenApiParameterExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Api.Generator.Extensions;
+using Microsoft.OpenApi.Models;
+
+namespace Azure.Api.Generator.OpenApi;
+
+internal static class OpenApiParameterExtensions
+{
+    internal static string GetTypeDeclarationIdentifier(this OpenApiParameter parameter) => 
+        parameter.Name.ToPascalCase() + parameter.In.ToString().ToPascalCase();
+}
