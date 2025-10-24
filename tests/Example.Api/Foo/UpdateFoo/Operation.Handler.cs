099f110e492605c6e198e8d794ac2ca126286819
Fredrik Arvidsson <fresa@fresa.se>
2025-10-24 11:30:18 +0200

    implement example handler

--- tests/Example.Api/Foo/UpdateFoo/Operation.Handler.cs
+++ tests/Example.Api/Foo/UpdateFoo/Operation.Handler.cs
@@ -1,10 +1,16 @@
-﻿namespace Example.Api.Foo.UpdateFoo;
+﻿using Example.Api.Foo.UpdateFoo.Content._200;
+
+namespace Example.Api.Foo.UpdateFoo;
 
 internal partial class Operation
 {
     internal partial Task<Response> HandleAsync(Request request, CancellationToken cancellationToken)
     {
-        var a = new Response.OK200();
-        throw new NotImplementedException();
+        var response = new Response.OK200
+        {
+            ApplicationJson = ApplicationJson.Create(
+                name: request.Body.ApplicationJson?.Name)
+        };
+        return Task.FromResult<Response>(response);
     }
 }
