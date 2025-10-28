f20e0e939418e0108ab5cfbfdf4e2287a6867407
Fredrik Arvidsson <fresa@fresa.se>
2025-10-28 15:56:26 +0100

    test(generator): assert exectuting an operation works as intended

--- tests/Example.Api.IntegrationTests/UpdateFooTests.cs
+++ tests/Example.Api.IntegrationTests/UpdateFooTests.cs
@@ -1,22 +1,26 @@
 ﻿using System.Net;
-using System.Text;
 using AwesomeAssertions;
+using Example.Api.IntegrationTests.Http;
+using Example.Api.IntegrationTests.Json;
 
 namespace Example.Api.IntegrationTests;
 
-public class UpdateFooTests(FooApplicationFactory app) : IClassFixture<FooApplicationFactory>
+public class UpdateFooTests(FooApplicationFactory app) : FooTestSpecification, IClassFixture<FooApplicationFactory>
 {
     [Fact]
     public async Task When_Updating_Foo_It_Should_Return_Updated_Foo()
     {
         using var client = app.CreateClient();
         var result = await client.PutAsync("/foo",
-            new StringContent(
+            CreateJsonContent(
                 """
                 {
                     "Name": "test"
                 }
-                """, encoding: Encoding.UTF8, mediaType: "application/json"));
+                """), CancellationToken);
         result.StatusCode.Should().Be(HttpStatusCode.OK);
+        var responseContent = await result.Content.ReadAsJsonNodeAsync(CancellationToken);
+        responseContent.Should().NotBeNull();
+        responseContent.GetValue<string>("#/Name").Should().Be("test");
     }
 }
